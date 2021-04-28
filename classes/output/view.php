6f0bd3fa2f3e5af6a6f08683e61b0de39ec95986
Richard Jones <richardnz@outlook.com>
2021-04-28 13:51:22 +1200

    Added a welcome message

--- classes/output/view.php
+++ classes/output/view.php
@@ -63,6 +63,7 @@ class view implements renderable, templatable {
         // Moodle handles processing of std intro field.
         $data->body = format_module_intro('collaborate',
                 $this->collaborate, $this->id);
+       $data->message = get_string('welcome', 'mod_collaborate');
 
         return $data;
     }
--- lang/en/collaborate.php
+++ lang/en/collaborate.php
@@ -45,3 +45,4 @@ $string['pluginname'] = 'collaborate';
 
 // Mod form specific collaborate settings.
 $string['title'] = 'Activity Title: ';
+$string['welcome'] = 'Welcome to Collaborate';
